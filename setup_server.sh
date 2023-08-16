b531a863e9106bdc7a8520b7e62fea897baba6cf
Dennis Shushack <dennis.shushack@ost.ch>
2023-08-16 14:35:45 +0200

    hello

--- setup_server.sh
+++ setup_server.sh
@@ -1,5 +1,20 @@
+#!/bin/bash
+
+# Install Screen with text:
+python3 setup.py 
+
+# Navigate to 'client' directory
 cd client
+
+# Set up a Python virtual environment
 python3 -m venv venv
+
+# Activate the virtual environment
 source venv/bin/activate
+
+# Install the required Python packages
 pip install -r requirements.txt
-deactivate
+
+# Deactivate the virtual environment
+python3 main.py
+
