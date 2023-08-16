23d141e850d430499cfb3c175cd8e5ee4b5e3875
Dennis Shushack <dennis.shushack@ost.ch>
2023-08-16 17:33:20 +0200

    hi

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 import socket
 import threading
@@ -12,11 +12,9 @@ class Data(db.Model):
     client = db.Column(db.String(50))
     temperature = db.Column(db.Float)
     humidity = db.Column(db.Float)
-    timestamp = db.Column(db.Float)
+    timestamp = db.Column(db.Float)  # Changed to Float
     room = db.Column(db.Integer)
 
-
-# Moved db.create_all() inside the app context
 with app.app_context():
     db.create_all()
 
@@ -28,40 +26,36 @@ sock.bind((UDP_IP, UDP_PORT))
 
 def listen_for_data():
     while True:
-        data, addr = sock.recvfrom(1024) 
-        temp, humidity, timestamp, room = map(float, data.decode().split(","))        
+        data, addr = sock.recvfrom(1024)
+        temp, humidity, timestamp, room = map(float, data.decode().split(","))
+        temp = round(temp, 2)
+        humidity = round(humidity, 2)
+        timestamp = round(timestamp, 2)
         new_data = Data(client=str(addr), temperature=temp, humidity=humidity, timestamp=timestamp, room=room)
-        with app.app_context():  # also wrap database operations inside the app context
+        with app.app_context():
             db.session.add(new_data)
             db.session.commit()
 
-        # Prints the data to the console
         print(f"Data received from client: {str(addr)}")
         print(f"Temperature: {temp}°C, Humidity: {humidity}%")
 
 @app.route('/')
 def index():
-    # Query all data from the database
-    all_data = Data.query.all()
+    rooms_data = {}
+    room_ids = db.session.query(Data.room).distinct().all()
+    for room_id in room_ids:
+        latest_entry = Data.query.filter_by(room=room_id[0]).order_by(Data.timestamp.desc()).first()
+        rooms_data[latest_entry.room] = latest_entry
 
-    # Group by room
-    rooms = {}
-    for entry in all_data:
-        if entry.room not in rooms:
-            rooms[entry.room] = []
-        rooms[entry.room].append(entry)
+    temps = [data.temperature for data in Data.query.order_by(Data.timestamp).all()]
+    times = [str(data.timestamp) for data in Data.query.order_by(Data.timestamp).all()]
 
-    # Render the data using a template
-    return render_template('index.html', rooms=rooms)
-
+    return render_template('index.html', rooms=rooms_data, temps=temps, times=times)
 
 def run_flask():
     app.run(host="0.0.0.0", port=8888)
-    
+
 if __name__ == "__main__":
-    # Starting Flask on a separate thread
     thread = threading.Thread(target=run_flask)
     thread.start()
-    
-    # Running the UDP listener
     listen_for_data()
