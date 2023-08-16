c1c63291e032f845bd77f08970b277383abd0311
Dennis Shushack <dennis.shushack@ost.ch>
2023-08-16 14:41:42 +0200

    s
    s

--- setup_client.sh
+++ setup_client.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
+
+# Navigate to 'client' directory
 cd client
+
+# Set up a Python virtual environment
 python3 -m venv venv
+
+# Activate the virtual environment
 source venv/bin/activate
+
+# Install the required Python packages
 pip install -r requirements.txt
-deactivate
-python setup.py 
+
+# Deactivate the virtual environment
+python3 setups.py
