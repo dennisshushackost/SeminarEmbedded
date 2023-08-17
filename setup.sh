8092ce84ae339a0178f7540390859968d4739edb
Dennis Shushack <dennis.shushack@ost.ch>
2023-08-17 13:56:54 +0200

    h3

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-cp home/pi/SeminarEmbedded/setup_client.service /etc/systemd/system/setup_client.service
+cp /home/pi/SeminarEmbedded/setup_client.service /etc/systemd/system/setup_client.service
 chmod +x /home/pi/SeminarEmbedded/setup_client.sh
 sudo systemctl daemon-reload
 sudo systemctl enable setup_client.service
